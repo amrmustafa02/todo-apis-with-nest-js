dfea496e6fe32fe1753aed807af9c562137b374c
“amrmustafa02” <amrmustafa6051@gmail.com>
2024-11-22 15:01:07 +0200

    add swagger to api

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import { GlobalErrorHandler } from './core/error/global-error-handler';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
+  
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Todo API') // API title
+    .setDescription('API documentation for the Todo application') // Description
+    .setVersion('1.0') // API version
+    .addBearerAuth() // Add Bearer Token authentication
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger UI available at /api
 
   app.useGlobalPipes(
     new ValidationPipe({
